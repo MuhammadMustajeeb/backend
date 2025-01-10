25b15ffe43cc104f698ed350a269d6463acf91ef
Muhammd Mustajeeb <mprogramming48@gmail.com>
2025-01-10 05:36:10 -0800

    add controllers, routes, app for healthcheck

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ import playlistRouter from './routes/playlist.routes.js';
 import likeRouter from './routes/like.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import tweetRouter from './route/tweet.routes.js';
+import healthcheckRouter from './routes/healthcheck.routes.js';
 
 //routes declaration, by using middleware
 app.use("/api/v1/users", userRouter);
@@ -34,6 +35,7 @@ app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 // http://localhost:8000/api/v1/users/register or login
 
